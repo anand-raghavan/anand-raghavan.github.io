c3241328903897718a08e1bfafd8065cc9937640
anand-raghavan <anand.r.7@gmail.com>
2022-10-14 20:57:37 +0530

    Update family.js
    First draft

--- family.js
+++ family.js
@@ -2,8 +2,8 @@
 'use strict';
 
 // Override these settings:
-var familyDataFilename = "simpsons-family.txt"; // Your own family.txt
-var defaultRootName = 'Leopold';                // Someone in your family
+var familyDataFilename = "kovaicousins-family.txt"; // Your own family.txt
+var defaultRootName = 'Ramakrishnan';                // Someone in your family
 var lineHeight = 280;  // 220 is better, but the Simpsons pngs are very vertical
 
 // Other rendering constants
